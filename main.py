0b5591792ea5c214bf3b4533320e5425d4d9cda6
Y1-18 <156465050+Y1-18@users.noreply.github.com>
2025-07-23 04:46:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from database import create_db_and_tables
 from routes import email_router, log_router
 from pydantic_settings import BaseSettings
 from fastapi.logger import logger
+from fastapi.responses import JSONResponse
+
 
 import nest_asyncio
 import uvicorn
@@ -17,7 +19,10 @@ async def lifespan(app: FastAPI):
     await create_db_and_tables()
     yield
 
-
+@app.get("/health")
+async def health_check():
+    return JSONResponse(content={"status": "healthy"})
+    
 app = FastAPI(lifespan=lifespan)
 app.include_router(email_router, prefix="/generate", tags=["Email"])
 app.include_router(log_router, prefix="/logs", tags=["Logs"])
